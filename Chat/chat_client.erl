3af4b1482115a30f7ca6e5684552d6d0577a1de8
Alexander Ivakin <smart2504@gmail.com>
2012-01-05 20:11:29 +0100

    Second episode

--- Chat/chat_client.erl
+++ Chat/chat_client.erl
@@ -1,5 +1,18 @@
 -module(chat_client).
--export([send_message/3]).
+-export([send_message/2, print_message/2, start_router/0,
+		 register_nickname/1, unregister_nickname/1]).
 
-send_message(RouterPid, Addressee, MessageBody) ->
-	message_router:send_chat_message(RouterPid, Addressee, MessageBody).
+register_nickname(Nickname) ->
+	message_router:register_nick(Nickname, fun(Msg) -> chat_client:print_message(Nickname, Msg) end).
+	
+unregister_nickname(Nickname) ->
+	message_router:unregister_nick(Nickname).   
+
+send_message(Addressee, MessageBody) ->
+	message_router:send_chat_message(Addressee, MessageBody).  
+	
+print_message(Who, MessageBody) ->
+	io:format("~p received: ~p~n", [Who, MessageBody]).         
+	
+start_router() ->
+	message_router:start().	
--- Chat/message_router.erl
+++ Chat/message_router.erl
@@ -1,26 +1,44 @@
 -module(message_router).
--export([start/0, stop/1, send_chat_message/3, route_messages/0]).
+-export([start/0, stop/0, send_chat_message/2, 
+		 register_nick/2, unregister_nick/1, route_messages/1]).
+-define(SERVER, message_router).
 
 start() ->
-	spawn(?MODULE, route_messages, []).
+	Pid = spawn(?MODULE, route_messages, [dict:new()]),
+	erlang:register(?SERVER, Pid).
 	
-stop(RouterPid) ->
-	RouterPid ! shutdown.	
+stop() ->
+	?SERVER ! shutdown.	
 
-send_chat_message(RouterPid, Addressee, MessageBody) ->
-	RouterPid ! {send_chat_msg, Addressee, MessageBody}.
+send_chat_message(Addressee, MessageBody) ->
+	?SERVER ! {send_chat_msg, Addressee, MessageBody}.
+	
+register_nick(ClientName, PrintFun) ->
+	?SERVER ! {register_nick, ClientName, PrintFun}.	
+	
+unregister_nick(ClientName) ->
+	?SERVER ! {unregister_nick, ClientName}.	
 
-route_messages() ->
-	receive 
-		{send_chat_msg, Addressee, MessageBody} ->
-			Addressee ! {recv_chat_msg, MessageBody},
-			route_messages();
-		{recv_chat_msg, MessageBody} -> 
-			io:format("Received: ~p~n", [MessageBody]),
-			route_messages();
+route_messages(Clients) ->
+	receive
+		{register_nick, ClientName, PrintFun} ->
+			route_messages(dict:store(ClientName, PrintFun, Clients)); 
+		{unregister_nick, ClientName} ->
+			route_messages(dict:erase(ClientName, Clients));	
+		{send_chat_msg, ClientName, MessageBody} ->
+			?SERVER ! {recv_chat_msg, ClientName, MessageBody},
+			route_messages(Clients);
+		{recv_chat_msg, ClientName, MessageBody} -> 
+			case dict:find(ClientName, Clients) of 
+				{ok, PrintFun} -> 
+					PrintFun(MessageBody);
+				error -> 
+				    io:format("Unknown client~n")
+			end,
+			route_messages(Clients);
 		shutdown ->
 			io:format("Shutting down~n");	
 		Unknown ->
 			io:format("Warning! Received: ~p~n", [Unknown]),
-			route_messages()
+			route_messages(Clients)
 	end.

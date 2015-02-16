710e2af1132d1561b89c80aa8ef4bd8755c68466
Alexander Ivakin <smart2504@gmail.com>
2015-02-16 21:45:39 +0100

    Documentation for the module and the function

--- Etudes/geom.erl
+++ Etudes/geom.erl
@@ -1,5 +1,15 @@
+%% @author Alexander Ivakin <alex.ivakin@icloud.com>
+%% @doc Functions for calculating areas of geometric shapes
+%% @copyright 2015 Alexander Ivakin
+%% @version 0.1
+
 -module(geom).
 -export([area/2]).
+
+%% @doc Calculates the area of a rectangle, given the
+%% length and width.
+
+-spec(area(number(), number()) -> number()).
 
 area(L, W) ->
   L * W.
